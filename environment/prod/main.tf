8c9b78e5239c062410bec345a10d157ddced7753
Faisal Khan <faisalkhanattari35@gmail.com>
2025-05-07 13:41:33 +0530

    Create main.tf

---
+++ environment/prod/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "eks" {
+  source              = "../../../../modules/eks"
+
+  cluster_name        = var.cluster_name
+  cluster_role_arn    = var.cluster_role_arn
+  kms_key_arn         = var.kms_key_arn
+  subnet_ids          = var.subnet_ids
+  security_group_ids  = var.security_group_ids
+  node_group_name     = var.node_group_name
+  node_role_arn       = var.node_role_arn
+  instance_type       = var.instance_type
+  desired_size        = var.desired_size
+  min_size            = var.min_size
+  max_size            = var.max_size
+  ec2_key_pair        = var.ec2_key_pair
+}
