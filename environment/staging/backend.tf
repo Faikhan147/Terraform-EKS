7f02f126a44deccc51e9054413504106fec7766f
Faisal Khan <faisalkhanattari35@gmail.com>
2025-05-07 18:10:28 +0530

    Update backend.tf

--- environment/staging/backend.tf
+++ environment/staging/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-backend"
+    bucket         = "terraform-backend-faisalkhan"
     key            = "eks/staging/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
