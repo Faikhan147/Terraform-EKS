d07307ee6d067e8af4c900c67d923b0d9b029144
Faisal Khan <faisalkhanattari35@gmail.com>
2025-05-07 21:26:29 +0530

    Update destroy-staging.sh

--- destroy-staging.sh
+++ destroy-staging.sh
@@ -7,7 +7,7 @@ echo "📦 Switching to staging workspace..."
 terraform workspace select staging
 
 echo "🛠️ Initializing Terraform..."
-terraform init
+terraform init -reconfigure
 
 echo "📝 Formatting Terraform files..."
 terraform fmt -recursive
