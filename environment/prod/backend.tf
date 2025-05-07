85f2fcd5bb6ef66be4a3048cde967467985cd5bb
Faisal Khan <faisalkhanattari35@gmail.com>
2025-05-07 19:54:29 +0530

    Update backend.tf

--- environment/prod/backend.tf
+++ environment/prod/backend.tf
@@ -4,6 +4,6 @@ terraform {
     key            = "eks/prod/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
-    dynamodb_table = "terraform-locks"
+    use_lockfile = "terraform-locks"
   }
 }
