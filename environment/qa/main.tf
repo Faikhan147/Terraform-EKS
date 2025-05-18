15bcf9f54ad7e365c57c5f8db44764b44048ddc9
Faisal Khan <faisalkhanattari35@gmail.com>
2025-05-18 21:11:18 +0530

    Update main.tf

--- environment/qa/main.tf
+++ environment/qa/main.tf
@@ -1,12 +1,24 @@
-module "eks" {
-  source              = "../../modules/eks"
+module "iam" {
+  source = "../../modules/iam"
 
-  cluster_name        = var.cluster_name
   eks_cluster_role_name                           = var.eks_cluster_role_name
   AmazonEKSClusterPolicy_arn                      = var.AmazonEKSClusterPolicy_arn
   AmazonEKSServicePolicy_arn                      = var.AmazonEKSServicePolicy_arn
   CloudWatchAgentServerPolicy_arn                 = var.CloudWatchAgentServerPolicy_arn
-  cluster_role_arn                                = var.cluster_role_arn
+
+  eks_nodes_ssm_role_name                           = var.eks_nodes_ssm_role_name
+  AmazonSSMManagedInstanceCore_arn                  = var.AmazonSSMManagedInstanceCore_arn
+  AmazonEKSWorkerNodePolicy_arn                     = var.AmazonEKSWorkerNodePolicy_arn
+  AmazonEKS_CNI_Policy_arn                          = var.AmazonEKS_CNI_Policy_arn
+  AmazonEC2ContainerRegistryReadOnly_arn            = var.AmazonEC2ContainerRegistryReadOnly_arn
+}
+
+
+module "eks" {
+  source              = "../../modules/eks"
+
+  cluster_name        = var.cluster_name
+  cluster_role_arn    = module.iam.eks_cluster_role_arn
   kms_key_arn         = var.kms_key_arn
   subnet_ids          = var.subnet_ids
   security_group_ids  = var.security_group_ids
@@ -15,9 +27,4 @@ module "eks" {
   desired_size        = var.desired_size
   min_size            = var.min_size
   max_size            = var.max_size
-  eks_nodes_ssm_role_name                           = var.eks_nodes_ssm_role_name
-  AmazonSSMManagedInstanceCore_arn                  = var.AmazonSSMManagedInstanceCore_arn
-  AmazonEKSWorkerNodePolicy_arn                     = var.AmazonEKSWorkerNodePolicy_arn
-  AmazonEKS_CNI_Policy_arn                          = var.AmazonEKS_CNI_Policy_arn
-  AmazonEC2ContainerRegistryReadOnly_arn            = var.AmazonEC2ContainerRegistryReadOnly_arn
 }
