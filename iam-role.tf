cde18ab5c3c28c7f915181044d84cb3c610afc46
Faisal Khan <faisalkhanattari35@gmail.com>
2025-05-18 19:20:33 +0530

    Update iam-role.tf

--- iam-role.tf
+++ iam-role.tf
@@ -1,3 +1,41 @@
+# EKS Cluster Role
+resource "aws_iam_role" "eks_cluster_role" {
+  name = var.eks_cluster_role_name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "eks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach AmazonEKSClusterPolicy
+resource "aws_iam_role_policy_attachment" "attach_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = var.AmazonEKSClusterPolicy_arn
+}
+
+# Attach AmazonEKSServicePolicy
+resource "aws_iam_role_policy_attachment" "attach_service_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = var.AmazonEKSServicePolicy_arn
+}
+
+# (Optional) Attach CloudWatchAgentServerPolicy
+resource "aws_iam_role_policy_attachment" "attach_cloudwatch_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = var.CloudWatchAgentServerPolicy_arn
+}
+
+
+
 # Role creation of EKS-Nodes
 
 resource "aws_iam_role" "eks_nodes_ssm_role" {
