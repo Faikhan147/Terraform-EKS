aa0b66b4f767d9c8aa0c7e2222cafab7e7490a18
Faisal Khan <faisalkhanattari35@gmail.com>
2025-05-18 20:22:28 +0530

    Update main.tf

--- environment/staging/main.tf
+++ environment/staging/main.tf
@@ -2,15 +2,22 @@ module "eks" {
   source              = "../../modules/eks"
 
   cluster_name        = var.cluster_name
-  cluster_role_arn    = var.cluster_role_arn
+  eks_cluster_role_name                           = var.eks_cluster_role_name
+  AmazonEKSClusterPolicy_arn                      = var.AmazonEKSClusterPolicy_arn
+  AmazonEKSServicePolicy_arn                      = var.AmazonEKSServicePolicy_arn
+  CloudWatchAgentServerPolicy_arn                 = var.CloudWatchAgentServerPolicy_arn
+  cluster_role_arn                                = var.cluster_role_arn
   kms_key_arn         = var.kms_key_arn
   subnet_ids          = var.subnet_ids
   security_group_ids  = var.security_group_ids
   node_group_name     = var.node_group_name
-  node_role_arn       = var.node_role_arn
   instance_type       = var.instance_type
   desired_size        = var.desired_size
   min_size            = var.min_size
   max_size            = var.max_size
-  ec2_key_pair        = var.ec2_key_pair
+  eks_nodes_ssm_role_name                           = var.eks_nodes_ssm_role_name
+  AmazonSSMManagedInstanceCore_arn                  = var.AmazonSSMManagedInstanceCore_arn
+  AmazonEKSWorkerNodePolicy_arn                     = var.AmazonEKSWorkerNodePolicy_arn
+  AmazonEKS_CNI_Policy_arn                          = var.AmazonEKS_CNI_Policy_arn
+  AmazonEC2ContainerRegistryReadOnly_arn            = var.AmazonEC2ContainerRegistryReadOnly_arn
 }
