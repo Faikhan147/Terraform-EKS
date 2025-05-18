ff410c4a39ce24a3298d64a5c0122265116b83e5
Faisal Khan <faisalkhanattari35@gmail.com>
2025-05-18 20:24:24 +0530

    Update variables.tf

--- environment/staging/variables.tf
+++ environment/staging/variables.tf
@@ -1,4 +1,8 @@
 variable "cluster_name" {}
+variable "eks_cluster_role_name" {}
+variable "AmazonEKSClusterPolicy_arn" {}
+variable "AmazonEKSServicePolicy_arn" {}
+variable "CloudWatchAgentServerPolicy_arn" {}
 variable "cluster_role_arn" {}
 variable "kms_key_arn" {}
 variable "subnet_ids" {
@@ -8,9 +12,12 @@ variable "security_group_ids" {
   type = list(string)
 }
 variable "node_group_name" {}
-variable "node_role_arn" {}
 variable "instance_type" {}
 variable "desired_size" {}
 variable "min_size" {}
 variable "max_size" {}
-variable "ec2_key_pair" {}
+variable "eks_nodes_ssm_role_name" {}
+variable "AmazonSSMManagedInstanceCore_arn" {}
+variable "AmazonEKSWorkerNodePolicy_arn" {}
+variable "AmazonEKS_CNI_Policy_arn" {}
+variable "AmazonEC2ContainerRegistryReadOnly_arn" {}
