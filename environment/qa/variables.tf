ee3ed5a33617ad1ff8b3625b0624802bb65fc301
Faisal Khan <faisalkhanattari35@gmail.com>
2025-05-18 21:11:45 +0530

    Update variables.tf

--- environment/qa/variables.tf
+++ environment/qa/variables.tf
@@ -1,9 +1,15 @@
-variable "cluster_name" {}
 variable "eks_cluster_role_name" {}
 variable "AmazonEKSClusterPolicy_arn" {}
 variable "AmazonEKSServicePolicy_arn" {}
 variable "CloudWatchAgentServerPolicy_arn" {}
-variable "cluster_role_arn" {}
+
+variable "eks_nodes_ssm_role_name" {}
+variable "AmazonSSMManagedInstanceCore_arn" {}
+variable "AmazonEKSWorkerNodePolicy_arn" {}
+variable "AmazonEKS_CNI_Policy_arn" {}
+variable "AmazonEC2ContainerRegistryReadOnly_arn" {}
+
+variable "cluster_name" {}
 variable "kms_key_arn" {}
 variable "subnet_ids" {
   type = list(string)
@@ -16,8 +22,3 @@ variable "instance_type" {}
 variable "desired_size" {}
 variable "min_size" {}
 variable "max_size" {}
-variable "eks_nodes_ssm_role_name" {}
-variable "AmazonSSMManagedInstanceCore_arn" {}
-variable "AmazonEKSWorkerNodePolicy_arn" {}
-variable "AmazonEKS_CNI_Policy_arn" {}
-variable "AmazonEC2ContainerRegistryReadOnly_arn" {}
