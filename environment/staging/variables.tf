b81d444d3985cfef6f50c7c2128f4d9b2732ed19
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-21 02:04:04 +0530

    Update variables.tf

--- environment/staging/variables.tf
+++ environment/staging/variables.tf
@@ -1,15 +1,15 @@
-variable "eks_cluster_role_name" {}
-variable "AmazonEKSClusterPolicy_arn" {}
-variable "AmazonEKSServicePolicy_arn" {}
-variable "CloudWatchAgentServerPolicy_arn" {}
+# variables.tf (real-time companies style)
 
-variable "eks_nodes_ssm_role_name" {}
-variable "AmazonSSMManagedInstanceCore_arn" {}
-variable "AmazonEKSWorkerNodePolicy_arn" {}
-variable "AmazonEKS_CNI_Policy_arn" {}
-variable "AmazonEC2ContainerRegistryReadOnly_arn" {}
 
+variable "env" {}
 variable "cluster_name" {}
+variable "node_group_name" {}
+variable "instance_type" {}
+variable "desired_size" {}
+variable "min_size" {}
+variable "max_size" {}
+variable "ami_id" {}
+variable "user_data" {}
 variable "kms_key_arn" {}
 variable "subnet_ids" {
   type = list(string)
@@ -17,11 +17,5 @@ variable "subnet_ids" {
 variable "security_group_ids" {
   type = list(string)
 }
-variable "node_group_name" {}
-variable "instance_type" {}
-variable "ami_id" {}
-variable "user_data" {}
-variable "env" {}
-variable "desired_size" {}
-variable "min_size" {}
-variable "max_size" {}
+variable "eks_cluster_role_arn" {}
+variable "eks_nodes_ssm_role_arn" {}
