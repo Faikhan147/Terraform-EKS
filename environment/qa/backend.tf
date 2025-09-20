a680fa5e2acefbd3395925d6fbe6996285b4ed7e
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-21 01:52:40 +0530

    Update backend.tf

--- environment/qa/backend.tf
+++ environment/qa/backend.tf
@@ -1,7 +1,9 @@
+# backend.tf (real-time companies style)
+
 terraform {
   backend "s3" {
-    bucket         = "terraform-backend-faisal-khan"
-    key            = "eks/qa/terraform.tfstate"
+    bucket         = "terraform-backend-all-env"
+    key            = "eks/prod/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
     dynamodb_table = "terraform-locks"
