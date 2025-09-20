cbc12bb72a600b18ac4255448f79afb85e9cb837
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-21 00:35:44 +0530

    Update main.tf

--- environment/prod/main.tf
+++ environment/prod/main.tf
@@ -1,35 +1,28 @@
-module "iam" {
-  source = "../../modules/iam"
-
-  eks_cluster_role_name                           = var.eks_cluster_role_name
-  AmazonEKSClusterPolicy_arn                      = var.AmazonEKSClusterPolicy_arn
-  AmazonEKSServicePolicy_arn                      = var.AmazonEKSServicePolicy_arn
-  CloudWatchAgentServerPolicy_arn                 = var.CloudWatchAgentServerPolicy_arn
-
-  eks_nodes_ssm_role_name                           = var.eks_nodes_ssm_role_name
-  AmazonSSMManagedInstanceCore_arn                  = var.AmazonSSMManagedInstanceCore_arn
-  AmazonEKSWorkerNodePolicy_arn                     = var.AmazonEKSWorkerNodePolicy_arn
-  AmazonEKS_CNI_Policy_arn                          = var.AmazonEKS_CNI_Policy_arn
-  AmazonEC2ContainerRegistryReadOnly_arn            = var.AmazonEC2ContainerRegistryReadOnly_arn
-}
-
+# main.tf (real-time companies style)
 
 module "eks" {
-  source              = "../../modules/eks"
+  source = "../../module/eks"
 
-  cluster_name        = var.cluster_name
-  cluster_role_arn    = module.iam.eks_cluster_role_arn
-  instance_profile_name = module.iam.eks_nodes_ssm_instance_profile_name
-  node_role_arn         = module.iam.eks_nodes_ssm_role_arn
-  kms_key_arn         = var.kms_key_arn
-  subnet_ids          = var.subnet_ids
-  security_group_ids  = var.security_group_ids
-  node_group_name     = var.node_group_name
-  instance_type       = var.instance_type
-  ami_id              = var.ami_id
-  user_data           = var.user_data
-  env                 = var.env
-  desired_size        = var.desired_size
-  min_size            = var.min_size
-  max_size            = var.max_size
+  env = var.env
+
+  # Cluster
+  cluster_name     = var.cluster_name
+  eks_cluster_role_arn = var.eks_cluster_role_arn  # pass ARN
+
+  # Node group
+  node_group_name       = var.node_group_name
+  instance_type         = var.instance_type
+  ami_id                = var.ami_id
+  desired_size          = var.desired_size
+  min_size              = var.min_size
+  max_size              = var.max_size
+  user_data             = var.user_data
+  eks_nodes_ssm_role_arn        = var.eks_nodes_ssm_role_arn   # pass ARN
+
+  # Networking
+  subnet_ids         = var.subnet_ids
+  security_group_ids = var.security_group_ids
+
+  # KMS
+  kms_key_arn = var.kms_key_arn
 }
