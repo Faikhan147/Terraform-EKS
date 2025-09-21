45b564f7c688223554309dc93cf73a146e8b68d5
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-21 20:12:00 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,12 +3,12 @@ provider "aws" {
 }
 
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.5.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.44"
+      version = "~> 5.0"
     }
   }
 }
