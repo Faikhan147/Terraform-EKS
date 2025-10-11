d790ccb82b01bb0dbbc5a7f57f78caa8abcf2c53
Faisal Khan <faisalkhanattari35@gmail.com>
2025-10-12 01:09:32 +0530

    Update main.tf

--- module/eks/main.tf
+++ module/eks/main.tf
@@ -38,8 +38,8 @@ resource "aws_launch_template" "eks_node_lt" {
   block_device_mappings {
     device_name = "/dev/xvda"
     ebs {
-      volume_size           = var.volume_size
-      volume_type           = var.volume_type
+      volume_size           = var.root_volume_size
+      volume_type           = var.root_volume_type
       delete_on_termination = true   # ✅ recommended
       encrypted             = true
       iops                  = var.root_volume_iops
