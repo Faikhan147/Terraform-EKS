01a3d4a64b2a689f41f495dea1c81805458a4985
Faisal Khan <faisalkhanattari35@gmail.com>
2025-10-12 02:21:48 +0530

    Update variables.tf

--- environment/qa/variables.tf
+++ environment/qa/variables.tf
@@ -10,11 +10,11 @@ variable "desired_size" {}
 variable "min_size" {}
 variable "max_size" {}
 variable "ami_id" {}
+variable "user_data" {}
 variable "root_volume_size" {}
 variable "root_volume_type" {}
 variable "root_volume_iops" {}
 variable "root_volume_throughput" {}
-variable "user_data" {}
 variable "kms_key_arn" {}
 variable "subnet_ids" {
   type = list(string)
